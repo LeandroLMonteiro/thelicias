5c8150011e9f6de28cb2fa4bbf9567276d5f9b08
leandro.monteiro <161344376+LeandroLMonteiro@users.noreply.github.com>
2025-04-28 17:26:10 -0300

    ultimos ajustes iniciais para Vercel

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
       POSTGRES_USER: process.env.POSTGRES_USER,
       POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
       POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-      DATABASE_PORT: process.env.DATABASE_PORT,
-      NODE_ENV: process.env.NODE_ENV
+      DATABASE_PORT: process.env.DATABASE_PORT
     }
   }
+
+  module.exports = nextConfig
